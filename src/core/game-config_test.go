1173fbfe1d25fea970d4623a5ce6fb11478bcc4c
henryleu <henryleu@126.com>
2019-08-18 12:31:39 +0800

    update

--- src/core/game-config_test.go
+++ src/core/game-config_test.go
@@ -5,8 +5,8 @@ import (
 	"testing"
 )
 
-func mockJudge() *Judge {
-	return &Judge{
+func mockGameConfig() *GameConfig {
+	return &GameConfig{
 		Width:     20,
 		Height:    20,
 		Vision:    4,
@@ -31,8 +31,8 @@ func mockTeamBattles() (tb1, tb2 TeamBattle) {
 	return
 }
 
-func TestJudge_NewBattle(t *testing.T) {
-	j := mockJudge()
+func TestGameConfig_NewBattle(t *testing.T) {
+	j := mockGameConfig()
 	tb1, tb2 := mockTeamBattles()
 	type fields struct {
 		TeamSeq       int
@@ -72,7 +72,7 @@ func TestJudge_NewBattle(t *testing.T) {
 				teamBattle2: tb2,
 			},
 			want: &GameBattle{
-				Judge:       j,
+				Config:      j,
 				TeamBattles: [2]TeamBattle{tb1, tb2},
 				TeamsPlayers: [2][]int{
 					[]int{1, 2, 3, 4},
@@ -84,7 +84,7 @@ func TestJudge_NewBattle(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			j := &Judge{
+			j := &GameConfig{
 				TeamSeq:     tt.fields.TeamSeq,
 				Width:       tt.fields.Width,
 				Height:      tt.fields.Height,
@@ -96,7 +96,7 @@ func TestJudge_NewBattle(t *testing.T) {
 				MapData:     tt.fields.MapData,
 			}
 			if got := j.NewBattle(tt.args.teamBattle1, tt.args.teamBattle2); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Judge.NewBattle() = %v, want %v", got, tt.want)
+				t.Errorf("GameConfig.NewBattle() = %v, want %v", got, tt.want)
 			}
 		})
 	}
